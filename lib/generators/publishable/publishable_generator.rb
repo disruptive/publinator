a6e8814021f210cf35e30c4740a47d00aa5585bc
Jerry Richardson <jerry@jerryr.com>
2012-10-23 23:39:40 -0400

    update generator

--- lib/generators/publishable/publishable_generator.rb
+++ lib/generators/publishable/publishable_generator.rb
@@ -1,12 +1,12 @@
 class PublishableGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
-  argument :class_name, :type => :string, :default => "article"
+  # argument :class_name, :type => :string, :default => "article"
 
   def create_publishable_initializer
     # create file for class
-    template "app/models/publishable_model.rb.erb", "app/models/#{file_name}.rb"
+    template "app/models/publishable_model.rb.erb", "app/models/#{name.classify}.rb"
 
     # create migration
-    template "app/models/publishable_migration.rb.erb", "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S").to_i}_create_#{class_name.pluralize}.rb"
+    template "app/models/publishable_migration.rb.erb", "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S").to_i}_create_#{name.tablelize}.rb"
   end
 end
